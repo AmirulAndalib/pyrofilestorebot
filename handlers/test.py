c38474895f712e22a4eb2289d37794568a7e4403
action00002 <113837903+action00002@users.noreply.github.com>
2022-11-15 20:54:32 +0530

    Update test.py

--- handlers/test.py
+++ handlers/test.py
@@ -45,14 +45,14 @@ from handlers.save_media import (
 
 MediaList = {}
 
-Bot = Client(
-    name=Config.BOT_USERNAME,
-    in_memory=True,
-    bot_token=Config.BOT_TOKEN,
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    workers = 4
-)
+# Bot = Client(
+#     name=Config.BOT_USERNAME,
+#     in_memory=True,
+#     bot_token=Config.BOT_TOKEN,
+#     api_id=Config.API_ID,
+#     api_hash=Config.API_HASH,
+#     workers = 4
+# )
 
 
 @Bot.on_message(filters.private)
